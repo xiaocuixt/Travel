655db0bc79dde11c28bacccff43e46896ff4eea0
xiaocuixt <cjw624923@126.com>
2018-05-01 12:57:32 +0800

    添加axios工具和mock数据

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,8 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import axios from 'axios'
+
 export default {
   name: 'Home',
   components: {
@@ -23,6 +25,17 @@ export default {
     HomeIcons,
     HomeRecommend,
     HomeWeekend
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
   }
 }
 </script>
