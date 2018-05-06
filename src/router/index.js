61872047e164f29ec32df59fe081438b832e4b6b
xiaocuixt <cjw624923@126.com>
2018-05-06 12:14:58 +0800

    js文件异步加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home' // @表示src目录
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
+// 该写法一次将js代码全部加载，当项目比较小时可以考虑，当项目比较大时，使用下面的异步加载js效果，每页根据需求加载
+// import Home from '@/pages/home/Home'
+// import City from '@/pages/city/City'
+// import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -11,17 +12,17 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/Home') // @表示src目录
     },
     {
       path: '/city',
       name: 'City',
-      component: City
+      component: () => import('@/pages/city/City')
     },
     {
       path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => import('@/pages/detail/Detail')
     }
   ],
   // 对于所有路由导航，简单地让页面滚动到顶部。
